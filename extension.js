707a8a32e4fa7fa0d135fa6b03c804deb25b418f
Christoph Bunte <christophbunte@gmail.com>
2018-12-26 16:53:48 +0100

    jump to definition now working

--- extension.js
+++ extension.js
@@ -5,8 +5,20 @@ const axios = require('axios')
 
 let errorRefreshIntervalObj = null
 let typeHoverDisposable = null
+let docHoverDisposable = null
 let typeCompletionDisposable = null
-//let scopeCompletionDisposable = null
+let scopeCompletionDisposable = null
+let definitionProviderDisposable = null
+
+function completionKindFromDetail(detail) {
+	const isDef = /\bdef\b/.test(detail)
+	const isVal = /\bval\b/.test(detail)
+	const isClass = /\b(class|object)\b/.test(detail)
+	return isDef ? vscode.CompletionItemKind.Method :
+		isVal ? vscode.CompletionItemKind.Field :
+			isClass ? vscode.CompletionItemKind.Class :
+				vscode.CompletionItemKind.Text
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -28,17 +40,41 @@ function activate(context) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
+		axios.post("http://localhost:9317/log-debug", {})
 	});
 
 	const diagnosticCollection = vscode.languages.createDiagnosticCollection("scalavista")
 
 	errorRefreshIntervalObj = setInterval(getErrorsAndUpdateDiagnostics, 1000)
 
-	const hoverProvider = {
-		provideHover(doc, pos, token) {
-			let filename = doc.fileName
-			let fileContents = doc.getText()
-			let offset = doc.offsetAt(pos)
+	const definitionProvider = {
+		provideDefinition(document, position, token) {
+
+			let filename = document.fileName
+			let fileContents = document.getText()
+			let offset = document.offsetAt(position)
+			let payload = { filename, fileContents, offset }
+			return axios.post("http://localhost:9317/ask-pos-at", payload).then(response => {
+				console.log(response.data)
+				let file = response.data.file
+				if (file == '<no source file>')
+					return null
+				let uri = vscode.Uri.file(file)
+				let pos = document.positionAt(parseInt(response.data.pos))
+				//console.log(file, uri, pos)
+				return new vscode.Location(uri, pos)
+			})
+
+		}
+	}
+
+	definitionProviderDisposable = vscode.languages.registerDefinitionProvider('scala', definitionProvider)
+
+	const typeHoverProvider = {
+		provideHover(document, position, token) {
+			let filename = document.fileName
+			let fileContents = document.getText()
+			let offset = document.offsetAt(position)
 			let payload = { filename, fileContents, offset }
 			return axios.post("http://localhost:9317/ask-type-at", payload).then(response => {
 				return new vscode.Hover(response.data)
@@ -46,19 +82,21 @@ function activate(context) {
 		}
 	}
 
-	typeHoverDisposable = vscode.languages.registerHoverProvider('scala', hoverProvider)
+	typeHoverDisposable = vscode.languages.registerHoverProvider('scala', typeHoverProvider)
 
-	function completionKindFromDetail(detail) {
-		if (/\bdef\b/.test(detail))
-			return vscode.CompletionItemKind.Method
-		if (/\b(class|object)\b/.text(detail))
-			return vscode.CompletionItemKind.Class
-		if (/\btrait\b/.test(detail))
-			return vscode.CompletionItemKind.Interface
-		if (/\bval\b/.test(detail))
-			return vscode.CompletionItemKind.Field
-		return vscode.CompletionItemKind.Text
+	const docHoverProvider = {
+		provideHover(doc, pos, token) {
+			let filename = doc.fileName
+			let fileContents = doc.getText()
+			let offset = doc.offsetAt(pos)
+			let payload = { filename, fileContents, offset }
+			return axios.post("http://localhost:9317/ask-doc-at", payload).then(response => {
+				return new vscode.Hover(response.data)
+			})
+		}
 	}
+
+	docHoverDisposable = vscode.languages.registerHoverProvider('scala', docHoverProvider)
 
 	const typeCompletionProvider = {
 
@@ -69,10 +107,10 @@ function activate(context) {
 			let payload = { filename, fileContents, offset }
 			return axios.post("http://localhost:9317/type-completion", payload).then(response => {
 				let completionItems = response.data.map(comp => {
-					let label = comp[0]
-					let detail = comp[1]
-					let kind = completionKindFromDetail(detail)
-					let item = new vscode.CompletionItem(label)
+					const label = comp[0]
+					const detail = comp[1]
+					const kind = completionKindFromDetail(detail)
+					const item = new vscode.CompletionItem(label, kind)
 					item.detail = detail
 					console.log('item' + item)
 					return item
@@ -83,30 +121,30 @@ function activate(context) {
 		}
 	}
 
-	typeCompletionDisposable = vscode.languages.registerCompletionItemProvider('scala', typeCompletionProvider)
+	typeCompletionDisposable = vscode.languages.registerCompletionItemProvider('scala', typeCompletionProvider, ".")
 
-	//const scopeCompletionProvider = {
+	const scopeCompletionProvider = {
 
-	//	provideCompletionItems(document, position, token, context) {
-	//		let filename = document.fileName
-	//		let fileContents = document.getText()
-	//		let offset = document.offsetAt(position)
-	//		let payload = { filename, fileContents, offset }
-	//		return axios.post("http://localhost:9317/scope-completion", payload).then(response => {
-	//			let completionItems = response.data.map(comp => {
-	//				let label = comp[0]
-	//				let detail = comp[1]
-	//				let kind = completionKindFromDetail(detail)
-	//				let item = new vscode.CompletionItem(label, kind)
-	//				item.detail = detail
-	//				return item
-	//			})
-	//			return new vscode.CompletionList(completionItems, true)
-	//		})
-	//	}
-	//}
+		provideCompletionItems(document, position, token, context) {
+			let filename = document.fileName
+			let fileContents = document.getText()
+			let offset = document.offsetAt(position)
+			let payload = { filename, fileContents, offset }
+			return axios.post("http://localhost:9317/scope-completion", payload).then(response => {
+				let completionItems = response.data.map(comp => {
+					let label = comp[0]
+					let detail = comp[1]
+					let kind = completionKindFromDetail(detail)
+					let item = new vscode.CompletionItem(label, kind)
+					item.detail = detail
+					return item
+				})
+				return new vscode.CompletionList(completionItems, true)
+			})
+		}
+	}
 
-	//scopeCompletionDisposable = vscode.languages.registerCompletionItemProvider('scala', scopeCompletionProvider)
+	scopeCompletionDisposable = vscode.languages.registerCompletionItemProvider('scala', scopeCompletionProvider)
 
 	function getErrorsAndUpdateDiagnostics() {
 
@@ -142,7 +180,7 @@ function activate(context) {
 				)
 				diagnosticCollection.set(uri, diagnostics)
 			})
-		})
+		}).catch(() => { })
 	}
 
 	vscode.workspace.onDidChangeTextDocument(event => {
@@ -159,8 +197,10 @@ exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() {
 	typeHoverDisposable.dispose()
+	docHoverDisposable.dispose()
 	typeCompletionDisposable.dispose()
-	//scopeCompletionDisposable.dispose()
+	scopeCompletionDisposable.dispose()
+	definitionProviderDisposable.dispose()
 	clearInterval(errorRefreshIntervalObj)
 }
 
